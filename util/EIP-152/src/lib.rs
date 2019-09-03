49e6be08fc544877655d63128a396761e756cf2e
Andronik Ordian <write@reusable.software>
2019-09-03 23:08:29 +0200

    eip-152: more readable and matches the reference impl

--- util/EIP-152/src/lib.rs
+++ util/EIP-152/src/lib.rs
@@ -18,16 +18,16 @@
 /// there are 10 16-byte arrays - one for each round
 /// the entries are calculated from the sigma constants.
 const SIGMA: [[usize; 16]; 10] = [
-	[0, 2, 4, 6, 1, 3, 5, 7, 8, 10, 12, 14, 9, 11, 13, 15],
-	[14, 4, 9, 13, 10, 8, 15, 6, 1, 0, 11, 5, 12, 2, 7, 3],
-	[11, 12, 5, 15, 8, 0, 2, 13, 10, 3, 7, 9, 14, 6, 1, 4],
-	[7, 3, 13, 11, 9, 1, 12, 14, 2, 5, 4, 15, 6, 10, 0, 8],
-	[9, 5, 2, 10, 0, 7, 4, 15, 14, 11, 6, 3, 1, 12, 8, 13],
-	[2, 6, 0, 8, 12, 10, 11, 3, 4, 7, 15, 1, 13, 5, 14, 9],
-	[12, 1, 14, 4, 5, 15, 13, 10, 0, 6, 9, 8, 7, 3, 2, 11],
-	[13, 7, 12, 3, 11, 14, 1, 9, 5, 15, 8, 2, 0, 4, 6, 10],
-	[6, 14, 11, 0, 15, 9, 3, 8, 12, 13, 1, 10, 2, 7, 4, 5],
-	[10, 8, 7, 1, 2, 4, 6, 5, 15, 9, 3, 13, 11, 14, 12, 0],
+	[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+	[14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
+	[11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
+	[7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
+	[9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
+	[2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9],
+	[12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
+	[13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
+	[6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
+	[10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0],
 ];
 
 
@@ -37,14 +37,44 @@ const IV: [u64; 8] = [
 	0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179,
 ];
 
+#[inline(always)]
+fn g(v: &mut [u64], a: usize, b: usize, c: usize, d: usize, x: u64, y: u64) {
+	v[a] = v[a].wrapping_add(v[b]).wrapping_add(x);
+	v[d] = (v[d] ^ v[a]).rotate_right(32);
+	v[c] = v[c].wrapping_add(v[d]);
+	v[b] = (v[b] ^ v[c]).rotate_right(24);
+	v[a] = v[a].wrapping_add(v[b]).wrapping_add(y);
+	v[d] = (v[d] ^ v[a]).rotate_right(16);
+	v[c] = v[c].wrapping_add(v[d]);
+	v[b] = (v[b] ^ v[c]).rotate_right(63);
+}
+
+#[inline(always)]
+fn round(r: usize, m: &[u64; 16], v: &mut [u64]) {
+    // Select the message schedule based on the round.
+    let s = SIGMA[r % 10];
+
+    // Mix the columns.
+    g(v, 0, 4, 8, 12, m[s[0]], m[s[1]]);
+    g(v, 1, 5, 9, 13, m[s[2]], m[s[3]]);
+    g(v, 2, 6, 10, 14, m[s[4]], m[s[5]]);
+    g(v, 3, 7, 11, 15, m[s[6]], m[s[7]]);
+
+    // Mix the rows.
+    g(v, 0, 5, 10, 15, m[s[8]], m[s[9]]);
+    g(v, 1, 6, 11, 12, m[s[10]], m[s[11]]);
+    g(v, 2, 7, 8, 13, m[s[12]], m[s[13]]);
+    g(v, 3, 4, 9, 14, m[s[14]], m[s[15]]);
+}
+
 /// F is a compression function for BLAKE2b. It takes as an argument the state
 /// vector `h`, message block vector `m`, offset counter `t`, final
 /// block indicator flag `f`, and number of rounds `rounds`. The state vector
 /// provided as the first parameter is modified by the function.
 pub fn compress(h: &mut [u64; 8], m: [u64; 16], t: [u64; 2], f: bool, rounds: usize) {
-	let mut v = Vec::new();
-	v.extend(h.iter().cloned());
-	v.extend_from_slice(&IV);
+	let mut v = [0u64; 16];
+	v[..h.len()].copy_from_slice(h);
+	v[h.len()..].copy_from_slice(&IV);
 
 	v[12] ^= t[0];
 	v[13] ^= t[1];
@@ -54,119 +84,7 @@ pub fn compress(h: &mut [u64; 8], m: [u64; 16], t: [u64; 2], f: bool, rounds: us
 	}
 
 	for i in 0..rounds {
-		let s = &SIGMA[i % 10];
-		v[0] = v[0].wrapping_add(m[s[0]]);
-		v[0] = v[0].wrapping_add(v[4]);
-		v[12] ^= v[0];
-		v[12] = v[12].rotate_right(32);
-		v[8] = v[8].wrapping_add(v[12]);
-		v[4] ^= v[8];
-		v[4] = v[4].rotate_right(24);
-		v[1] = v[1].wrapping_add(m[s[1]]);
-		v[1] = v[1].wrapping_add(v[5]);
-		v[13] ^= v[1];
-		v[13] = v[13].rotate_right(32);
-		v[9] = v[9].wrapping_add(v[13]);
-		v[5] ^= v[9];
-		v[5] = v[5].rotate_right(24);
-		v[2] = v[2].wrapping_add(m[s[2]]);
-		v[2] = v[2].wrapping_add(v[6]);
-		v[14] ^= v[2];
-		v[14] = v[14].rotate_right(32);
-		v[10] = v[10].wrapping_add(v[14]);
-		v[6] ^= v[10];
-		v[6] = v[6].rotate_right(24);
-		v[3] = v[3].wrapping_add(m[s[3]]);
-		v[3] = v[3].wrapping_add(v[7]);
-		v[15] ^= v[3];
-		v[15] = v[15].rotate_right(32);
-		v[11] = v[11].wrapping_add(v[15]);
-		v[7] ^= v[11];
-		v[7] = v[7].rotate_right(24);
-		v[0] = v[0].wrapping_add(m[s[4]]);
-		v[0] = v[0].wrapping_add(v[4]);
-		v[12] ^= v[0];
-		v[12] = v[12].rotate_right(16);
-		v[8] = v[8].wrapping_add(v[12]);
-		v[4] ^= v[8];
-		v[4] = v[4].rotate_right(63);
-		v[1] = v[1].wrapping_add(m[s[5]]);
-		v[1] = v[1].wrapping_add(v[5]);
-		v[13] ^= v[1];
-		v[13] = v[13].rotate_right(16);
-		v[9] = v[9].wrapping_add(v[13]);
-		v[5] ^= v[9];
-		v[5] = v[5].rotate_right(63);
-		v[2] = v[2].wrapping_add(m[s[6]]);
-		v[2] = v[2].wrapping_add(v[6]);
-		v[14] ^= v[2];
-		v[14] = v[14].rotate_right(16);
-		v[10] = v[10].wrapping_add(v[14]);
-		v[6] ^= v[10];
-		v[6] = v[6].rotate_right(63);
-		v[3] = v[3].wrapping_add(m[s[7]]);
-		v[3] = v[3].wrapping_add(v[7]);
-		v[15] ^= v[3];
-		v[15] = v[15].rotate_right(16);
-		v[11] = v[11].wrapping_add(v[15]);
-		v[7] ^= v[11];
-		v[7] = v[7].rotate_right(63);
-		v[0] = v[0].wrapping_add(m[s[8]]);
-		v[0] = v[0].wrapping_add(v[5]);
-		v[15] ^= v[0];
-		v[15] = v[15].rotate_right(32);
-		v[10] = v[10].wrapping_add(v[15]);
-		v[5] ^= v[10];
-		v[5] = v[5].rotate_right(24);
-		v[1] = v[1].wrapping_add(m[s[9]]);
-		v[1] = v[1].wrapping_add(v[6]);
-		v[12] ^= v[1];
-		v[12] = v[12].rotate_right(32);
-		v[11] = v[11].wrapping_add(v[12]);
-		v[6] ^= v[11];
-		v[6] = v[6].rotate_right(24);
-		v[2] = v[2].wrapping_add(m[s[10]]);
-		v[2] = v[2].wrapping_add(v[7]);
-		v[13] ^= v[2];
-		v[13] = v[13].rotate_right(32);
-		v[8] = v[8].wrapping_add(v[13]);
-		v[7] ^= v[8];
-		v[7] = v[7].rotate_right(24);
-		v[3] = v[3].wrapping_add(m[s[11]]);
-		v[3] = v[3].wrapping_add(v[4]);
-		v[14] ^= v[3];
-		v[14] = v[14].rotate_right(32);
-		v[9] = v[9].wrapping_add(v[14]);
-		v[4] ^= v[9];
-		v[4] = v[4].rotate_right(24);
-		v[0] = v[0].wrapping_add(m[s[12]]);
-		v[0] = v[0].wrapping_add(v[5]);
-		v[15] ^= v[0];
-		v[15] = v[15].rotate_right(16);
-		v[10] = v[10].wrapping_add(v[15]);
-		v[5] ^= v[10];
-		v[5] = v[5].rotate_right(63);
-		v[1] = v[1].wrapping_add(m[s[13]]);
-		v[1] = v[1].wrapping_add(v[6]);
-		v[12] ^= v[1];
-		v[12] = v[12].rotate_right(16);
-		v[11] = v[11].wrapping_add(v[12]);
-		v[6] ^= v[11];
-		v[6] = v[6].rotate_right(63);
-		v[2] = v[2].wrapping_add(m[s[14]]);
-		v[2] = v[2].wrapping_add(v[7]);
-		v[13] ^= v[2];
-		v[13] = v[13].rotate_right(16);
-		v[8] = v[8].wrapping_add(v[13]);
-		v[7] ^= v[8];
-		v[7] = v[7].rotate_right(63);
-		v[3] = v[3].wrapping_add(m[s[15]]);
-		v[3] = v[3].wrapping_add(v[4]);
-		v[14] ^= v[3];
-		v[14] = v[14].rotate_right(16);
-		v[9] = v[9].wrapping_add(v[14]);
-		v[4] ^= v[9];
-		v[4] = v[4].rotate_right(63);
+		round(i, &m, &mut v[..]);
 	}
 
 	for i in 0..8 {
